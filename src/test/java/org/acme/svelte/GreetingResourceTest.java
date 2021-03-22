cdd982fd31c6f1b58bcacc9c85eede417f678523
Fred Bricon <fbricon@gmail.com>
2021-03-22 12:53:46 +0100

    Small test tweak
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/test/java/org/acme/svelte/GreetingResourceTest.java
+++ src/test/java/org/acme/svelte/GreetingResourceTest.java
@@ -4,18 +4,19 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
+        String name = "fred";
         given()
-          .when().get("/hello/fred")
+          .when().get("/hello/"+name)
           .then()
              .statusCode(200)
-             .body(containsString(" fred"));
+             .body(endsWith(" "+name));
     }
 
 }
